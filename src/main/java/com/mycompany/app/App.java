2ca5858fd22d88db626c0e51fffbc8c6b7787d40
wangzhengjia <578292013@qq.com>
2019-04-02 13:50:07 +0800

    jenkins test1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-		System.out.println(new App().getMessage() + "test");
+		System.out.println(new App().getMessage() + "test2");
     }
 
     private final String getMessage() {
